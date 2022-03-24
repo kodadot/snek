44e369ab174ec380329d48dd0e116a86a068ec29
Viki Val <viktorko99@gmail.com>
2022-03-24 14:36:19 +0100

    :zap: added events and extrinsics to process

---
+++ src/processable.ts
@@ -0,0 +1,28 @@
+export enum Event { 
+  createClass = 'nft.ClassCreated',
+  createInstance = 'nft.InstanceMinted',
+  transfer = 'nft.InstanceTransferred',
+  burn = 'nft.InstanceBurned',
+  destroy = 'nft.ClassDestroyed',
+  priceUpdate = 'marketplace.TokenPriceUpdated',
+  sold = 'marketplace.InstanceSold',
+  placeOffer = 'marketplace.OfferPlaced',
+  withdrawOffer = 'marketplace.OfferWithdrawn',
+  acceptOffer = 'marketplace.OfferAccepted',
+  payRoyalty = 'marketplace.RoyaltyPaid',
+  addRoyalty = 'marketplace.RoyaltyAdded',
+}
+
+export enum Extrinsic {
+  createClass = 'nft.createClass',
+  mint = 'nft.mint',
+  transfer = 'nft.transfer',
+  burn = 'nft.burn',
+  destroyClass = 'nft.destroyClass',
+  acceptOffer = 'marketplace.acceptOffer',
+  addRoyalty = 'marketplace.addRoyalty',
+  buy = 'marketplace.buy',
+  makeOffer = 'marketplace.makeOffer',
+  setPrice = 'marketplace.setPrice',
+  withdrawOffer = 'marketplace.withdrawOffer',
+}
