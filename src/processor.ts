3fe894de0d82d29dd68c7b2922705f9eb8545016
Viki Val <viktorko99@gmail.com>
2022-03-25 14:39:25 +0100

    :rocket: processor has correct events to index

--- src/processor.ts
+++ src/processor.ts
@@ -1,13 +1,10 @@
-import * as ss58 from "@subsquid/ss58";
 import {
-  EventHandlerContext,
-  Store,
   SubstrateProcessor,
 } from "@subsquid/substrate-processor";
 import { lookupArchive } from "@subsquid/archive-registry";
-import { Account, HistoricalBalance } from "./model";
-import { BalancesTransferEvent } from "./types/events";
+import { Event } from './processable'
 import logger from './mappings/utils/logger'
+import * as mappings from './mappings'
 
 const processor = new SubstrateProcessor("snek_nft");
 
@@ -22,80 +19,19 @@ processor.setDataSource({
   chain: "wss://basilisk-kodadot.hydration.cloud",
 });
 
-processor.addEventHandler("balances.Transfer", async (ctx) => {
-  const transfer = getTransferEvent(ctx);
-  const tip = ctx.extrinsic?.tip || 0n;
-  const from = ss58.codec("basilisk").encode(transfer.from);
-  const to = ss58.codec("basilisk").encode(transfer.to);
+const dummy = async () => {}
 
-  const fromAcc = await getOrCreate(ctx.store, Account, from);
-  fromAcc.balance = fromAcc.balance || 0n;
-  fromAcc.balance -= transfer.amount;
-  fromAcc.balance -= tip;
-  await ctx.store.save(fromAcc);
-
-  const toAcc = await getOrCreate(ctx.store, Account, to);
-  toAcc.balance = toAcc.balance || 0n;
-  toAcc.balance += transfer.amount;
-  await ctx.store.save(toAcc);
-
-  await ctx.store.save(
-    new HistoricalBalance({
-      id: `${ctx.event.id}-to`,
-      account: fromAcc,
-      balance: fromAcc.balance,
-      date: new Date(ctx.block.timestamp),
-    })
-  );
-
-  await ctx.store.save(
-    new HistoricalBalance({
-      id: `${ctx.event.id}-from`,
-      account: toAcc,
-      balance: toAcc.balance,
-      date: new Date(ctx.block.timestamp),
-    })
-  );
-});
+processor.addEventHandler(Event.createClass, mappings.handleCollectionCreate);
+processor.addEventHandler(Event.createInstance, mappings.handleTokenCreate);
+processor.addEventHandler(Event.transfer, mappings.handleTokenTransfer);
+processor.addEventHandler(Event.burn, mappings.handleTokenBurn);
+processor.addEventHandler(Event.destroy, mappings.handleCollectionDestroy);
+// processor.addEventHandler(Event.priceUpdate, dummy);
+// processor.addEventHandler(Event.sold, dummy);
+// processor.addEventHandler(Event.placeOffer, dummy);
+// processor.addEventHandler(Event.withdrawOffer, dummy);
+// processor.addEventHandler(Event.acceptOffer, dummy);
+// processor.addEventHandler(Event.payRoyalty, dummy);
+// processor.addEventHandler(Event.addRoyalty, dummy);
 
 processor.run();
-
-interface TransferEvent {
-  from: Uint8Array;
-  to: Uint8Array;
-  amount: bigint;
-}
-
-function getTransferEvent(ctx: EventHandlerContext): TransferEvent {
-  const event = new BalancesTransferEvent(ctx);
-  if (event.isV1020) {
-    const [from, to, amount] = event.asV1020;
-    return { from, to, amount };
-  }
-  if (event.isV1050) {
-    const [from, to, amount] = event.asV1050;
-    return { from, to, amount };
-  }
-  return event.asLatest;
-}
-
-async function getOrCreate<T extends { id: string }>(
-  store: Store,
-  EntityConstructor: EntityConstructor<T>,
-  id: string
-): Promise<T> {
-  let entity = await store.get<T>(EntityConstructor, {
-    where: { id },
-  });
-
-  if (entity == null) {
-    entity = new EntityConstructor();
-    entity.id = id;
-  }
-
-  return entity;
-}
-
-type EntityConstructor<T> = {
-  new (...args: any[]): T;
-};
