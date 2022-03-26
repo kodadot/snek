f3d0b6796cdfff862df3077e1ca93feadf827e00
Viki Val <viktorko99@gmail.com>
2022-03-26 17:34:30 +0100

    :zap: added unwrap event function

--- src/mappings/utils/extract.ts
+++ src/mappings/utils/extract.ts
@@ -1,21 +1,29 @@
 
-import { ExtrinsicHandlerContext, SubstrateExtrinsic, SubstrateEvent, EventHandlerContext } from '@subsquid/substrate-processor'
-import { BaseCall } from './types'
+import { BaseCall, CallWith, Context, UnwrapFunc } from './types'
 
 
-function toBaseCall(extrinsic: ExtrinsicHandlerContext): BaseCall {
-  const caller = extrinsic.extrinsic.signer.toString();
-  const blockNumber = extrinsic.block.height.toString();
-  const timestamp = new Date(extrinsic.block.timestamp);
+// function toBaseCall(extrinsic: ExtrinsicHandlerContext): BaseCall {
+//   const caller = extrinsic.extrinsic.signer.toString();
+//   const blockNumber = extrinsic.block.height.toString();
+//   const timestamp = new Date(extrinsic.block.timestamp);
+
+//   return { caller, blockNumber, timestamp };
+// }
+
+
+function toBaseEvent(event: Context): BaseCall {
+  const caller = event.extrinsic?.signer.toString() || ''; 
+  const blockNumber = event.block.height.toString();
+  const timestamp = new Date(event.block.timestamp);
 
   return { caller, blockNumber, timestamp };
 }
 
-// function toBaseEvent(event: EventHandlerContext): BaseCall {
-//   const caller = event.caller;
-//   const blockNumber = event.blockNumber.toString();
-//   const timestamp = new Date(event.timestamp);
 
-//   return { caller, blockNumber, timestamp };
-// }
+export function unwrap<T>(ctx: Context, unwrapFn: UnwrapFunc<T>): CallWith<T> {
+  const baseCall = toBaseEvent(ctx);
+  const unwrapped = unwrapFn(ctx);
+  return { ...baseCall, ...unwrapped };
+}
+
 
