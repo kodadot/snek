348f4a06ca1c44c3d46f976a4997b96d5636cc3c
Viki Val <viktorko99@gmail.com>
2022-03-31 15:53:46 +0200

    :construction: :test_tube: Trying some tests for consolidator

--- tests/basic.spec.ts
+++
@@ -1,9 +0,0 @@
-import { assert, describe, it } from 'vitest'
-
-describe('skipped suite', () => {
-  it('test', () => {
-    // Suite skipped, no error
-    assert.equal(Math.sqrt(4), 2)
-  })
-})
-
