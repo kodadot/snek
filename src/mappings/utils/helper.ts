f96c87143aa47cd518747513cf8725402fb38165
Viki Val <viktorko99@gmail.com>
2022-04-04 13:18:22 +0200

    :zap: added helper to properly format the addresses

--- src/mappings/utils/helper.ts
+++ src/mappings/utils/helper.ts
@@ -1,4 +1,10 @@
+import * as ss58 from "@subsquid/ss58";
+
 export function isEmpty(obj: Record<string, any>) {
   for (const _ in obj) { return false; }
   return true;
+}
+
+export function addressOf(address: Uint8Array) {
+ return ss58.codec("basilisk").encode(address);
 }
