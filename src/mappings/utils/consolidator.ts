204e77fb6f5f4d99d187369f1a13398bfb1a441d
Viki Val <viktorko99@gmail.com>
2022-05-21 16:13:29 +0200

    :zap: consolidator accepts <T extends AbstractEntity>

--- src/mappings/utils/consolidator.ts
+++ src/mappings/utils/consolidator.ts
@@ -1,7 +1,7 @@
-import { CollectionEntity, NFTEntity } from '../../model/generated'
-import logger from './logger'
+import { CollectionEntity, NFTEntity, Offer } from '../../model/generated'
 
 type Entity = CollectionEntity | NFTEntity
+type AbstractEntity = Entity | Offer
 
 export function real<T>(entity: T | undefined): boolean {
   return !!entity
@@ -15,13 +15,17 @@ export function created(entity: Entity): boolean {
   return Object.keys(entity).length === 1 && entity.id !== undefined
 }
 
-export function entityOf(entity: Entity): string {
+export function entityOf(entity: AbstractEntity): string {
   if (entity instanceof CollectionEntity) {
     return 'Collection'
   }
 
   if (entity instanceof NFTEntity) {
     return 'NFT'
+  }
+
+  if (entity instanceof Offer) {
+    return 'Offer'
   }
 
   return ''
@@ -31,15 +35,15 @@ export function remintable(entity: Entity): boolean {
   return burned(entity) || created(entity)
 }
 
-export function plsBe<T extends Entity>(callback: (arg: T) => boolean, entity: T) {
+export function plsBe<T extends AbstractEntity>(callback: (arg: T) => boolean, entity: T) {
   return needTo(callback, entity, true)
 }
 
-export function plsNotBe<T extends Entity>(callback: (arg: T) => boolean, entity: T) {
+export function plsNotBe<T extends AbstractEntity>(callback: (arg: T) => boolean, entity: T) {
   return needTo(callback, entity, false)
 }
 
-export function needTo<T extends Entity>(callback: (arg: T) => boolean, entity: T, positive: boolean = true) {
+export function needTo<T extends AbstractEntity>(callback: (arg: T) => boolean, entity: T, positive: boolean = true) {
   const entityName = entityOf(entity)
   if (positive ? !callback(entity) : callback(entity)) {
     throw new ReferenceError(`[PROBLEM] Entity ${entityName} needs ${positive ? '' : 'not'} to be ${callback.name}`)
