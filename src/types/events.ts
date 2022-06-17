bf1d2bf52f123df272cab40dce66cae7b4b57adb
Viki Val <viktorko99@gmail.com>
2022-06-17 13:50:47 +0200

    :top: chain got new types since reset

--- src/types/events.ts
+++ src/types/events.ts
@@ -1,9 +1,6 @@
 import assert from 'assert'
 import {EventContext, Result, deprecateLatest} from './support'
-import * as v39 from './v39'
-import * as v50 from './v50'
-import * as v51 from './v51'
-import * as v52 from './v52'
+import * as v48 from './v48'
 
 export class MarketplaceOfferAcceptedEvent {
   constructor(private ctx: EventContext) {
@@ -11,43 +8,28 @@ export class MarketplaceOfferAcceptedEvent {
   }
 
   /**
-   * Offer was accepted \[sender, class_id, instance_id\]
-   */
-  get isV39(): boolean {
-    return this.ctx._chain.getEventHash('marketplace.OfferAccepted') === '426271b0ff71255c125e9a4ea897d86d39682c8454bbff4c6c9a8d50e0d966a4'
-  }
-
-  /**
-   * Offer was accepted \[sender, class_id, instance_id\]
-   */
-  get asV39(): [v39.AccountId32, bigint, bigint, bigint] {
-    assert(this.isV39)
-    return this.ctx._chain.decodeEvent(this.ctx.event)
-  }
-
-  /**
    * Offer was accepted \[sender, class_id, instance_id, amount, maker\]
    */
-  get isV52(): boolean {
+  get isV48(): boolean {
     return this.ctx._chain.getEventHash('marketplace.OfferAccepted') === '83fa789650cf7e0a68ea0f67c2a68a8e0de3f500393a77592cb755fdb5787c36'
   }
 
   /**
    * Offer was accepted \[sender, class_id, instance_id, amount, maker\]
    */
-  get asV52(): [v52.AccountId32, bigint, bigint, bigint, v52.AccountId32] {
-    assert(this.isV52)
+  get asV48(): [v48.AccountId32, bigint, bigint, bigint, v48.AccountId32] {
+    assert(this.isV48)
     return this.ctx._chain.decodeEvent(this.ctx.event)
   }
 
   get isLatest(): boolean {
     deprecateLatest()
-    return this.isV52
+    return this.isV48
   }
 
-  get asLatest(): [v52.AccountId32, bigint, bigint, bigint, v52.AccountId32] {
+  get asLatest(): [v48.AccountId32, bigint, bigint, bigint, v48.AccountId32] {
     deprecateLatest()
-    return this.asV52
+    return this.asV48
   }
 }
 
@@ -57,43 +39,28 @@ export class MarketplaceOfferPlacedEvent {
   }
 
   /**
-   * Offer was placed on a token \[offerer, class_id, instance_id, price\]
-   */
-  get isV39(): boolean {
-    return this.ctx._chain.getEventHash('marketplace.OfferPlaced') === '426271b0ff71255c125e9a4ea897d86d39682c8454bbff4c6c9a8d50e0d966a4'
-  }
-
-  /**
-   * Offer was placed on a token \[offerer, class_id, instance_id, price\]
-   */
-  get asV39(): [v39.AccountId32, bigint, bigint, bigint] {
-    assert(this.isV39)
-    return this.ctx._chain.decodeEvent(this.ctx.event)
-  }
-
-  /**
    * Offer was placed on a token \[offerer, class_id, instance_id, price, expires\]
    */
-  get isV51(): boolean {
+  get isV48(): boolean {
     return this.ctx._chain.getEventHash('marketplace.OfferPlaced') === '0c0020b8a59f4c44bfafff6516e075c67efa07d49d2257040c27bd47de251831'
   }
 
   /**
    * Offer was placed on a token \[offerer, class_id, instance_id, price, expires\]
    */
-  get asV51(): [v51.AccountId32, bigint, bigint, bigint, number] {
-    assert(this.isV51)
+  get asV48(): [v48.AccountId32, bigint, bigint, bigint, number] {
+    assert(this.isV48)
     return this.ctx._chain.decodeEvent(this.ctx.event)
   }
 
   get isLatest(): boolean {
     deprecateLatest()
-    return this.isV51
+    return this.isV48
   }
 
-  get asLatest(): [v51.AccountId32, bigint, bigint, bigint, number] {
+  get asLatest(): [v48.AccountId32, bigint, bigint, bigint, number] {
     deprecateLatest()
-    return this.asV51
+    return this.asV48
   }
 }
 
@@ -105,26 +72,26 @@ export class MarketplaceOfferWithdrawnEvent {
   /**
    * Offer was withdrawn \[sender, class_id, instance_id\]
    */
-  get isV39(): boolean {
+  get isV48(): boolean {
     return this.ctx._chain.getEventHash('marketplace.OfferWithdrawn') === '0f263bfdefa394edfb38d20d33662423a2e0902235b599f9b2b0292f157f0902'
   }
 
   /**
    * Offer was withdrawn \[sender, class_id, instance_id\]
    */
-  get asV39(): [v39.AccountId32, bigint, bigint] {
-    assert(this.isV39)
+  get asV48(): [v48.AccountId32, bigint, bigint] {
+    assert(this.isV48)
     return this.ctx._chain.decodeEvent(this.ctx.event)
   }
 
   get isLatest(): boolean {
     deprecateLatest()
-    return this.isV39
+    return this.isV48
   }
 
-  get asLatest(): [v39.AccountId32, bigint, bigint] {
+  get asLatest(): [v48.AccountId32, bigint, bigint] {
     deprecateLatest()
-    return this.asV39
+    return this.asV48
   }
 }
 
@@ -136,26 +103,26 @@ export class MarketplaceRoyaltyAddedEvent {
   /**
    * Marketplace data has been added \[class_type, sender, class_id, instance_id\]
    */
-  get isV39(): boolean {
+  get isV48(): boolean {
     return this.ctx._chain.getEventHash('marketplace.RoyaltyAdded') === 'b25c5b1351882b8049f26b3ffe8318b0c04beabe7f3b1174b983af490abf68f7'
   }
 
   /**
    * Marketplace data has been added \[class_type, sender, class_id, instance_id\]
    */
-  get asV39(): [bigint, bigint, v39.AccountId32, number] {
-    assert(this.isV39)
+  get asV48(): [bigint, bigint, v48.AccountId32, number] {
+    assert(this.isV48)
     return this.ctx._chain.decodeEvent(this.ctx.event)
   }
 
   get isLatest(): boolean {
     deprecateLatest()
-    return this.isV39
+    return this.isV48
   }
 
-  get asLatest(): [bigint, bigint, v39.AccountId32, number] {
+  get asLatest(): [bigint, bigint, v48.AccountId32, number] {
     deprecateLatest()
-    return this.asV39
+    return this.asV48
   }
 }
 
@@ -167,26 +134,26 @@ export class MarketplaceRoyaltyPaidEvent {
   /**
    * Royalty hs been paid to the author \[class_id, instance_id, author, royalty, royalty_amount\]
    */
-  get isV39(): boolean {
+  get isV48(): boolean {
     return this.ctx._chain.getEventHash('marketplace.RoyaltyPaid') === '82293205d464a489606def2289dde2ad7444a78cb3ae19f599a2160d68a0b720'
   }
 
   /**
    * Royalty hs been paid to the author \[class_id, instance_id, author, royalty, royalty_amount\]
    */
-  get asV39(): [bigint, bigint, v39.AccountId32, number, bigint] {
-    assert(this.isV39)
+  get asV48(): [bigint, bigint, v48.AccountId32, number, bigint] {
+    assert(this.isV48)
     return this.ctx._chain.decodeEvent(this.ctx.event)
   }
 
   get isLatest(): boolean {
     deprecateLatest()
-    return this.isV39
+    return this.isV48
   }
 
-  get asLatest(): [bigint, bigint, v39.AccountId32, number, bigint] {
+  get asLatest(): [bigint, bigint, v48.AccountId32, number, bigint] {
     deprecateLatest()
-    return this.asV39
+    return this.asV48
   }
 }
 
@@ -198,26 +165,26 @@ export class MarketplaceTokenPriceUpdatedEvent {
   /**
    * The price for a token was updated \[owner, class_id, instance_id, price\]
    */
-  get isV39(): boolean {
+  get isV48(): boolean {
     return this.ctx._chain.getEventHash('marketplace.TokenPriceUpdated') === '4100700286e3b39a636551e9e9872940d3c125d1b8729ac058742455e638fbe2'
   }
 
   /**
    * The price for a token was updated \[owner, class_id, instance_id, price\]
    */
-  get asV39(): [v39.AccountId32, bigint, bigint, (bigint | undefined)] {
-    assert(this.isV39)
+  get asV48(): [v48.AccountId32, bigint, bigint, (bigint | undefined)] {
+    assert(this.isV48)
     return this.ctx._chain.decodeEvent(this.ctx.event)
   }
 
   get isLatest(): boolean {
     deprecateLatest()
-    return this.isV39
+    return this.isV48
   }
 
-  get asLatest(): [v39.AccountId32, bigint, bigint, (bigint | undefined)] {
+  get asLatest(): [v48.AccountId32, bigint, bigint, (bigint | undefined)] {
     deprecateLatest()
-    return this.asV39
+    return this.asV48
   }
 }
 
@@ -229,26 +196,26 @@ export class MarketplaceTokenSoldEvent {
   /**
    * Token was sold to a new owner \[owner, buyer, class_id, instance_id, price\]
    */
-  get isV39(): boolean {
+  get isV48(): boolean {
     return this.ctx._chain.getEventHash('marketplace.TokenSold') === '4a3bc2182538af0cb911036daeda76c419c2f42491eda8f66b9ca681035507c0'
   }
 
   /**
    * Token was sold to a new owner \[owner, buyer, class_id, instance_id, price\]
    */
-  get asV39(): [v39.AccountId32, v39.AccountId32, bigint, bigint, bigint] {
-    assert(this.isV39)
+  get asV48(): [v48.AccountId32, v48.AccountId32, bigint, bigint, bigint] {
+    assert(this.isV48)
     return this.ctx._chain.decodeEvent(this.ctx.event)
   }
 
   get isLatest(): boolean {
     deprecateLatest()
-    return this.isV39
+    return this.isV48
   }
 
-  get asLatest(): [v39.AccountId32, v39.AccountId32, bigint, bigint, bigint] {
+  get asLatest(): [v48.AccountId32, v48.AccountId32, bigint, bigint, bigint] {
     deprecateLatest()
-    return this.asV39
+    return this.asV48
   }
 }
 
@@ -260,41 +227,26 @@ export class NftClassCreatedEvent {
   /**
    * A class was created \[owner, class_id, class_type\]
    */
-  get isV39(): boolean {
-    return this.ctx._chain.getEventHash('nft.ClassCreated') === '7adeb3f2ae9b2b9c39201542a741e44b5484fadd52179e412e45be77a794f225'
-  }
-
-  /**
-   * A class was created \[owner, class_id, class_type\]
-   */
-  get asV39(): {owner: v39.AccountId32, classId: bigint, classType: v39.ClassType} {
-    assert(this.isV39)
-    return this.ctx._chain.decodeEvent(this.ctx.event)
-  }
-
-  /**
-   * A class was created \[owner, class_id, class_type\]
-   */
-  get isV50(): boolean {
+  get isV48(): boolean {
     return this.ctx._chain.getEventHash('nft.ClassCreated') === '964234ae203d3207b740072bc8630eee21c72fe7995f3fc03e62f0bb443cca32'
   }
 
   /**
    * A class was created \[owner, class_id, class_type\]
    */
-  get asV50(): {owner: v50.AccountId32, classId: bigint, classType: v50.ClassType, metadata: Uint8Array} {
-    assert(this.isV50)
+  get asV48(): {owner: v48.AccountId32, classId: bigint, classType: v48.ClassType, metadata: Uint8Array} {
+    assert(this.isV48)
     return this.ctx._chain.decodeEvent(this.ctx.event)
   }
 
   get isLatest(): boolean {
     deprecateLatest()
-    return this.isV50
+    return this.isV48
   }
 
-  get asLatest(): {owner: v50.AccountId32, classId: bigint, classType: v50.ClassType, metadata: Uint8Array} {
+  get asLatest(): {owner: v48.AccountId32, classId: bigint, classType: v48.ClassType, metadata: Uint8Array} {
     deprecateLatest()
-    return this.asV50
+    return this.asV48
   }
 }
 
@@ -306,26 +258,26 @@ export class NftClassDestroyedEvent {
   /**
    * A class was destroyed \[class_id\]
    */
-  get isV39(): boolean {
+  get isV48(): boolean {
     return this.ctx._chain.getEventHash('nft.ClassDestroyed') === '51309f98603f5eeb2eb07f9373848f1874c4bfaea4a29b0e0d21dd93b98da94a'
   }
 
   /**
    * A class was destroyed \[class_id\]
    */
-  get asV39(): {owner: v39.AccountId32, classId: bigint} {
-    assert(this.isV39)
+  get asV48(): {owner: v48.AccountId32, classId: bigint} {
+    assert(this.isV48)
     return this.ctx._chain.decodeEvent(this.ctx.event)
   }
 
   get isLatest(): boolean {
     deprecateLatest()
-    return this.isV39
+    return this.isV48
   }
 
-  get asLatest(): {owner: v39.AccountId32, classId: bigint} {
+  get asLatest(): {owner: v48.AccountId32, classId: bigint} {
     deprecateLatest()
-    return this.asV39
+    return this.asV48
   }
 }
 
@@ -337,26 +289,26 @@ export class NftInstanceBurnedEvent {
   /**
    * An instance was burned \[sender, class_id, instance_id\]
    */
-  get isV39(): boolean {
+  get isV48(): boolean {
     return this.ctx._chain.getEventHash('nft.InstanceBurned') === 'cbf0740ecac063f0cc91759153cc494f3d948025e716ccd16da079129444cc1d'
   }
 
   /**
    * An instance was burned \[sender, class_id, instance_id\]
    */
-  get asV39(): {owner: v39.AccountId32, classId: bigint, instanceId: bigint} {
-    assert(this.isV39)
+  get asV48(): {owner: v48.AccountId32, classId: bigint, instanceId: bigint} {
+    assert(this.isV48)
     return this.ctx._chain.decodeEvent(this.ctx.event)
   }
 
   get isLatest(): boolean {
     deprecateLatest()
-    return this.isV39
+    return this.isV48
   }
 
-  get asLatest(): {owner: v39.AccountId32, classId: bigint, instanceId: bigint} {
+  get asLatest(): {owner: v48.AccountId32, classId: bigint, instanceId: bigint} {
     deprecateLatest()
-    return this.asV39
+    return this.asV48
   }
 }
 
@@ -368,41 +320,26 @@ export class NftInstanceMintedEvent {
   /**
    * An instance was minted \[owner, class_id, instance_id\]
    */
-  get isV39(): boolean {
-    return this.ctx._chain.getEventHash('nft.InstanceMinted') === 'cbf0740ecac063f0cc91759153cc494f3d948025e716ccd16da079129444cc1d'
-  }
-
-  /**
-   * An instance was minted \[owner, class_id, instance_id\]
-   */
-  get asV39(): {owner: v39.AccountId32, classId: bigint, instanceId: bigint} {
-    assert(this.isV39)
-    return this.ctx._chain.decodeEvent(this.ctx.event)
-  }
-
-  /**
-   * An instance was minted \[owner, class_id, instance_id\]
-   */
-  get isV50(): boolean {
+  get isV48(): boolean {
     return this.ctx._chain.getEventHash('nft.InstanceMinted') === 'eb2d7da6cd031b1051bd4c0ebcbe8cd70b244f54737e21a7f8279dccee6fa006'
   }
 
   /**
    * An instance was minted \[owner, class_id, instance_id\]
    */
-  get asV50(): {owner: v50.AccountId32, classId: bigint, instanceId: bigint, metadata: Uint8Array} {
-    assert(this.isV50)
+  get asV48(): {owner: v48.AccountId32, classId: bigint, instanceId: bigint, metadata: Uint8Array} {
+    assert(this.isV48)
     return this.ctx._chain.decodeEvent(this.ctx.event)
   }
 
   get isLatest(): boolean {
     deprecateLatest()
-    return this.isV50
+    return this.isV48
   }
 
-  get asLatest(): {owner: v50.AccountId32, classId: bigint, instanceId: bigint, metadata: Uint8Array} {
+  get asLatest(): {owner: v48.AccountId32, classId: bigint, instanceId: bigint, metadata: Uint8Array} {
     deprecateLatest()
-    return this.asV50
+    return this.asV48
   }
 }
 
@@ -414,25 +351,25 @@ export class NftInstanceTransferredEvent {
   /**
    * An instance was transferred \[from, to, class_id, instance_id\]
    */
-  get isV39(): boolean {
+  get isV48(): boolean {
     return this.ctx._chain.getEventHash('nft.InstanceTransferred') === 'e0a071978a33a540c15a46174c5018087ae648a19419f54dab0cb069ce949563'
   }
 
   /**
    * An instance was transferred \[from, to, class_id, instance_id\]
    */
-  get asV39(): {from: v39.AccountId32, to: v39.AccountId32, classId: bigint, instanceId: bigint} {
-    assert(this.isV39)
+  get asV48(): {from: v48.AccountId32, to: v48.AccountId32, classId: bigint, instanceId: bigint} {
+    assert(this.isV48)
     return this.ctx._chain.decodeEvent(this.ctx.event)
   }
 
   get isLatest(): boolean {
     deprecateLatest()
-    return this.isV39
+    return this.isV48
   }
 
-  get asLatest(): {from: v39.AccountId32, to: v39.AccountId32, classId: bigint, instanceId: bigint} {
+  get asLatest(): {from: v48.AccountId32, to: v48.AccountId32, classId: bigint, instanceId: bigint} {
     deprecateLatest()
-    return this.asV39
+    return this.asV48
   }
 }
