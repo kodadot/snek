e7a35ff944609de178d5b7bca0a369e40b3212c2
petersopko <petersopko@protonmail.com>
2022-06-22 15:38:29 +0200

    smol_stuff

--- src/server-extension/query/activeWallets.ts
+++ src/server-extension/query/activeWallets.ts
@@ -1,2 +1,3 @@
-export const activeWallets = `SELECT COUNT(DISTINCT e.caller) as total
-FROM event e`;
+export const activeWallets = `
+    SELECT COUNT(DISTINCT e.caller) as total
+    FROM event e`;
--- src/server-extension/resolvers/index.ts
+++ src/server-extension/resolvers/index.ts
@@ -1,26 +1,4 @@
-import { Field, ObjectType, Query, Resolver } from "type-graphql";
-import type { EntityManager } from "typeorm";
 import { WalletResolver } from "./walletResolver";
 import { OfferStatsResolver } from "./offerStatsResolver";
-
-@ObjectType()
-export class Hello {
-  @Field(() => String, { nullable: false })
-  greeting!: string;
-
-  constructor(greeting: string) {
-    this.greeting = greeting;
-  }
-}
-
-@Resolver()
-export class HelloResolver {
-  constructor(private tx: () => Promise<EntityManager>) {}
-
-  @Query(() => Hello)
-  async hello(): Promise<Hello> {
-    return new Hello(`Hey, this is you custom API extension`);
-  }
-}
 
 export { WalletResolver, OfferStatsResolver };
