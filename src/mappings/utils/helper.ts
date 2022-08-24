ff290d5af06027b6fb8e6ad68f487cf9aa23cab7
Viki Val <viktorko99@gmail.com>
2022-08-24 17:46:44 +0200

    :zap: addressOF can unwrap PK string

--- src/mappings/utils/helper.ts
+++ src/mappings/utils/helper.ts
@@ -1,4 +1,5 @@
 import * as ss58 from '@subsquid/ss58';
+import { decodeHex } from '@subsquid/substrate-processor';
 import { SomethingWithOptionalMeta } from './types';
 
 export function isEmpty(obj: Record<string, any>) {
@@ -6,8 +7,9 @@ export function isEmpty(obj: Record<string, any>) {
   return true;
 }
 
-export function addressOf(address: Uint8Array): string {
-  return ss58.codec('basilisk').encode(address);
+export function addressOf(address: Uint8Array | string): string {
+  const value = typeof address === 'string' ? decodeHex(address) : address;
+  return ss58.codec('basilisk').encode(value);
 }
 
 export function metadataOf({ metadata }: SomethingWithOptionalMeta): string {
