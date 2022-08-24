3c725be580ea7aac7fd88b6efbb6d67b19771d1b
Viki Val <viktorko99@gmail.com>
2022-08-24 16:08:59 +0200

    :squid: assets in :fire:

--- src/mappings/assetRegistry/index.ts
+++ src/mappings/assetRegistry/index.ts
@@ -3,22 +3,22 @@ import { AssetEntity } from '../../model';
 import { create, get } from '../utils/entity';
 import { unwrap } from '../utils/extract';
 import logger from '../utils/logger';
-import { Context } from '../utils/types';
+import { Context, Store } from '../utils/types';
 import { getAssetMetadataEvent, getAssetRegisterEvent, getAssetUpdateEvent } from './getters';
 
-export async function forceCreateBasiliskAsset(context: BlockHandlerContext): Promise<void> {
+export async function forceCreateBasiliskAsset(context: BlockHandlerContext<Store>): Promise<void> {
   logger.pending(`[REGISTER BASILISK]: ${context.block.height}`);
   const asset = create<AssetEntity>(AssetEntity, '0', {
     name: 'BSX',
     symbol: 'BSX',
     decimals: 12,
   });
-  logger.success(`[ASSET REGISTER]: by ${asset.id} is ${asset.name}`);
+  logger.success(`[ASSET REGISTER]: by ${asset.id} is ${asset.name || ''}`);
   await context.store.save<AssetEntity>(asset);
 }
 
 export async function handleAssetRegister(context: Context): Promise<void> {
-  logger.pending(`[ASSET REGISTER]: ${context.event.blockNumber}`);
+  logger.pending(`[ASSET REGISTER]: ${context.block.height}`);
   const event = unwrap(context, getAssetRegisterEvent);
   const asset = create<AssetEntity>(AssetEntity, event.id, {});
 
@@ -33,7 +33,7 @@ export async function handleAssetRegister(context: Context): Promise<void> {
 }
 
 export async function handleAssetUpdate(context: Context): Promise<void> {
-  logger.pending(`[ASSET UPDATE]: ${context.event.blockNumber}`);
+  logger.pending(`[ASSET UPDATE]: ${context.block.height}`);
   const event = unwrap(context, getAssetUpdateEvent);
   const asset = await get<AssetEntity>(context.store, AssetEntity, event.id);
   if (!asset) {
@@ -52,7 +52,7 @@ export async function handleAssetUpdate(context: Context): Promise<void> {
 }
 
 export async function handleAssetMetadata(context: Context): Promise<void> {
-  logger.pending(`[ASSET METADATA]: ${context.event.blockNumber}`);
+  logger.pending(`[ASSET METADATA]: ${context.block.height}`);
   const event = unwrap(context, getAssetMetadataEvent);
   const asset = await get<AssetEntity>(context.store, AssetEntity, event.id);
   if (!asset) {
