0a70334490ff69750008a8e0b6a4ee4436d63eb0
Preschian Febryantara <preschian27@gmail.com>
2022-09-09 21:55:49 +0700

    add collection in lastEvent

--- src/server-extension/query/event.ts
+++ src/server-extension/query/event.ts
@@ -7,14 +7,17 @@ export const lastEventQuery = `SELECT
     me.image as image,
     me.animation_url,
     MAX(e.timestamp) as timestamp,
-    MAX(e.meta) as value
+    MAX(e.meta) as value,
+    ne.collection_id as collection_id,
+    ce.name as collection_name
 
 FROM event e
     JOIN nft_entity ne on e.nft_id = ne.id
     LEFT join metadata_entity me on me.id = ne.metadata
+    LEFT JOIN collection_entity ce on ne.collection_id = ce.id
 WHERE
     e.interaction = $1
     AND ne.burned = false
-GROUP BY ne.id, me.id, e.current_owner, me.image
+GROUP BY ne.id, me.id, e.current_owner, me.image, ce.name
 ORDER BY MAX(e.timestamp) DESC
-LIMIT $2 OFFSET $3`
+LIMIT $2 OFFSET $3`;
