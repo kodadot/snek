8ac309835f78892ac8bf93f4ef84a010c3791568
Viki Val <viktorko99@gmail.com>
2022-09-28 16:05:02 +0200

    :soap: eslint for series.model

--- src/server-extension/model/series.model.ts
+++ src/server-extension/model/series.model.ts
@@ -1,55 +1,55 @@
-import { Field, ObjectType } from 'type-graphql'
+import { Field, ObjectType } from 'type-graphql';
 
 @ObjectType()
 export class SeriesEntity {
-	@Field(() => String, { nullable: false })
-	id!: string
+  @Field(() => String, { nullable: false })
+  id!: string;
 
-	@Field(() => String, { nullable: false })
-	issuer!: string
+  @Field(() => String, { nullable: false })
+  issuer!: string;
 
-	@Field(() => Number, { nullable: false })
-	unique!: number
+  @Field(() => Number, { nullable: false })
+  unique!: number;
 
-	@Field(() => Number, { nullable: false, name: 'uniqueCollectors' })
-	unique_collectors!: number
+  @Field(() => Number, { nullable: false, name: 'uniqueCollectors' })
+  unique_collectors!: number;
 
-	@Field(() => Number, { nullable: false })
-	sold!: number
+  @Field(() => Number, { nullable: false })
+  sold!: number;
 
-	@Field(() => Number, { nullable: false })
-	total!: number
+  @Field(() => Number, { nullable: false })
+  total!: number;
 
-	@Field(() => BigInt, { nullable: true, defaultValue: 0n, name: 'averagePrice' })
-	average_price!: bigint
+  @Field(() => BigInt, { nullable: true, defaultValue: 0n, name: 'averagePrice' })
+  average_price!: bigint;
 
-	@Field(() => BigInt, { nullable: true, defaultValue: 0n, name: 'floorPrice' })
-	floor_price!: bigint
+  @Field(() => BigInt, { nullable: true, defaultValue: 0n, name: 'floorPrice' })
+  floor_price!: bigint;
 
-	@Field(() => BigInt, { nullable: true, defaultValue: 0n, name: 'highestSale' })
-	highest_sale!: bigint
+  @Field(() => BigInt, { nullable: true, defaultValue: 0n, name: 'highestSale' })
+  highest_sale!: bigint;
 
-	@Field(() => Number, { nullable: true, defaultValue: 0 })
-	buys!: number
+  @Field(() => Number, { nullable: true, defaultValue: 0 })
+  buys!: number;
 
-	// @Field(() => Number, { nullable: true, defaultValue: 0 })
-	// rank!: number
+  // @Field(() => Number, { nullable: true, defaultValue: 0 })
+  // rank!: number
 
-	@Field(() => BigInt, { nullable: true, defaultValue: 0n })
-	volume!: bigint
+  @Field(() => BigInt, { nullable: true, defaultValue: 0n })
+  volume!: bigint;
 
-	@Field(() => String, { nullable: false })
-	name!: string
+  @Field(() => String, { nullable: false })
+  name!: string;
 
-	@Field(() => String, { nullable: true })
-	metadata!: string
+  @Field(() => String, { nullable: true })
+  metadata!: string;
 
-	@Field(() => String, { nullable: true })
-	image!: string
+  @Field(() => String, { nullable: true })
+  image!: string;
 
-	constructor(props: Partial<SeriesEntity>) {
-		Object.assign(this, props)
-	}
+  constructor(props: Partial<SeriesEntity>) {
+    Object.assign(this, props);
+  }
 }
 
 //         dailyVolume: { $sum: '$dailyVolume' },
