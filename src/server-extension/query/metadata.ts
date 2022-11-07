2b374445a240de536fbe0e43b7512de421bbd0b6
Viki Val <viktorko99@gmail.com>
2022-11-07 17:30:28 +0100

    :alien: query the magic data

--- src/server-extension/query/metadata.ts
+++ src/server-extension/query/metadata.ts
@@ -1,4 +1,5 @@
-export const emptyMetadata = `
-    SELECT distinct metadata
+export const missingMetadata = `
+    SELECT distinct ne.metadata as uri
     FROM nft_entity ne
-    WHERE ne.meta_id IS NULL;`;
+    WHERE ne.meta_id IS NULL
+    AND ne.metadata IS NOT NULL;`;
