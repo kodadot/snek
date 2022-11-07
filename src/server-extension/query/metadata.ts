6286c9e23abdbdb9d713ca9a06854d0faa188701
Viki Val <viktorko99@gmail.com>
2022-11-07 17:34:04 +0100

    :alien: update metadata via SQL

--- src/server-extension/query/metadata.ts
+++ src/server-extension/query/metadata.ts
@@ -3,3 +3,9 @@ export const missingMetadata = `
     FROM nft_entity ne
     WHERE ne.meta_id IS NULL
     AND ne.metadata IS NOT NULL;`;
+
+export const updateMissingMetadata = `
+    UPDATE nft_entity ne
+    SET meta_id = me.id
+    FROM metadata_entity me
+    WHERE ne.metadata = me.id`;
