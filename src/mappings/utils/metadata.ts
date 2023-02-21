f07a3ed2b94c2988ea93f53dc0e1743bc3e58c9e
Viki Val <viktorko99@gmail.com>
2023-02-21 21:17:34 +0100

    :zap: handle metadata view properly

--- src/mappings/utils/metadata.ts
+++ src/mappings/utils/metadata.ts
@@ -1,9 +1,10 @@
-import { TokenMetadata } from '@kodadot1/metasquid/types'
+import { TokenMetadata } from '@kodadot1/metasquid/types';
 import { $obtain } from '@kodadot1/minipfs';
 import { MetadataEntity } from '../../model';
 import { EntityWithId } from './entity';
 import logger from './logger';
 import { attributeFrom, ensure } from './types';
+import { CollectionType } from '../../model/generated/_collectionType';
 
 export const fetchMetadata = async <T>(metadata: string): Promise<T> => {
   try {
@@ -40,3 +41,19 @@ export const makeCompatibleMetadata = (id: string, metadata: TokenMetadata): Par
   name: metadata.name,
   type: metadata.type || '',
 });
+
+export const ensureMetadataUri = (metadata: string | undefined, collectionType: CollectionType): string => {
+  if (metadata) {
+    return metadata;
+  }
+
+  switch (collectionType) {
+    case CollectionType.LiquidityMining:
+    case CollectionType.Redeemable:
+    case CollectionType.Auction:
+    case CollectionType.HydraHeads:
+      return 'ipfs://ipfs/bafkreig447xngd2gnk24xvh6ycaivqeeegbjhgwuqcaoknlq5zq7oedf4a';
+    default:
+      return 'ipfs://ipfs/bafkreignxq5tnh2myt3gfa3neisd3uw36ap5kkcf5afxlrbuv45dzhs5ke';
+  }
+};
