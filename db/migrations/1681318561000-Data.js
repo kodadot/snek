ef07e3c4aceca3c000a3673f3de799cb53776513
Alko89 <alefer89@gmail.com>
2023-04-12 19:09:04 +0200

    fix: migration

--- db/migrations/1681318561000-Data.js
+++ db/migrations/1681318561000-Data.js
@@ -3,11 +3,11 @@ module.exports = class Data1681318561000 {
 
     async up(db) {
         await db.query(`ALTER TABLE "collection_entity" ADD "volume" numeric`)
-        await db.query(`ALTER TABLE "collection_entity" ADD "highestSalePrice" numeric`)
+        await db.query(`ALTER TABLE "collection_entity" ADD "highest_sale_price" numeric`)
     }
 
     async down(db) {
         await db.query(`ALTER TABLE "collection_entity" DROP COLUMN "volume"`)
-        await db.query(`ALTER TABLE "collection_entity" DROP COLUMN "highestSalePrice"`)
+        await db.query(`ALTER TABLE "collection_entity" DROP COLUMN "highest_sale_price"`)
     }
 }
