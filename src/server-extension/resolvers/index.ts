d0438054b26e2dff0d396dc3ae5c0ad060a5bcc4
Alko89 <alefer89@gmail.com>
2023-04-17 17:49:34 +0200

    fix: remove comment and shorten result query

--- src/server-extension/resolvers/index.ts
+++ src/server-extension/resolvers/index.ts
@@ -4,10 +4,8 @@ import { OfferStatsResolver } from './offerStatsResolver';
 import { EventResolver } from './event';
 import { SeriesResolver } from './series';
 import { SpotlightResolver } from './spotlight';
-// import { CollectionResolver } from './collectionResolver';
 
 export {
-  // CollectionResolver,
   CollectionEventResolver,
   WalletResolver,
   OfferStatsResolver,
