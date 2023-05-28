38d6123812503469007e4427a0533ab5d43a561e
Viki Val <viktorko99@gmail.com>
2023-05-28 14:16:27 +0200

    :label: dual chain config

---
+++ src/types/basilisk/v25.ts
@@ -0,0 +1,12 @@
+import type {Result, Option} from './support'
+
+export type AssetType = AssetType_Token | AssetType_PoolShare
+
+export interface AssetType_Token {
+    __kind: 'Token'
+}
+
+export interface AssetType_PoolShare {
+    __kind: 'PoolShare'
+    value: [number, number]
+}
---
+++ src/types/basilisk/v71.ts
@@ -0,0 +1,23 @@
+import type {Result, Option} from './support'
+
+export type ClassType = ClassType_Marketplace | ClassType_LiquidityMining | ClassType_Redeemable | ClassType_Auction | ClassType_HydraHeads
+
+export interface ClassType_Marketplace {
+    __kind: 'Marketplace'
+}
+
+export interface ClassType_LiquidityMining {
+    __kind: 'LiquidityMining'
+}
+
+export interface ClassType_Redeemable {
+    __kind: 'Redeemable'
+}
+
+export interface ClassType_Auction {
+    __kind: 'Auction'
+}
+
+export interface ClassType_HydraHeads {
+    __kind: 'HydraHeads'
+}
