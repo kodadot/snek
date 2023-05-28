9d31143279151d154233c7958bdc7da08454a8d1
Viki Val <viktorko99@gmail.com>
2023-05-28 14:18:29 +0200

    :wrench: get prod by chain name

--- src/environment.ts
+++ src/environment.ts
@@ -1,4 +1,4 @@
-const VERSION = process.env.VERSION || '004';
+export const CHAIN = process.env.CHAIN || 'basilisk';
 // Prod
 const ARCHIVE_URL = 'https://basilisk-firesquid.play.hydration.cloud/graphql';
 const NODE_URL = 'wss://rpc.basilisk.cloud';
@@ -7,7 +7,7 @@ const NODE_URL = 'wss://rpc.basilisk.cloud';
 const DEV_ARCHIVE_URL = 'https://basilisk-rococo-firesquid.play.hydration.cloud/graphql';
 const DEV_NODE_URL = 'wss://basilisk-rococo-rpc.play.hydration.cloud';
 
-const isProd = Number(VERSION) >= 5;
+export const isProd = CHAIN === 'basilisk';
 
 export const getArchiveUrl = (): string => (isProd ? ARCHIVE_URL : DEV_ARCHIVE_URL);
 export const getNodeUrl = (): string => (isProd ? NODE_URL : DEV_NODE_URL);
